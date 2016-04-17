cf763806d394113f053a8583d0d70a7c275c61c1
Thiago <thiagoisaiasbrg@gmail.com>
2016-04-17 17:30:25 -0300

    Initial file - Task 03

---
+++ 03_GaussLegendre.cpp
@@ -0,0 +1,13 @@
+/* Implementar e testar as quadraturas de Gauss-Legendre para integração de funções.
+O método que retorna o valor da função para um dado x deve ser programado e incorporado ao código.
+A entrada de dados deve ser:
+1) Valor do início do intervalo de integração;
+2) Valor do final do intervalo de integração;
+3) Valor da tolerância (potência negativa de 10).
+O output deve ser o valor da integral da função para os limites de integração fornecidos.
+O usuário deve ter a opção de escolher o número de pontos de Legendre a serem usados no somatório: 1 a 5.
+Anexe também um arquivo pdf com os seguintes desenvolvimentos:
+1) O polinômio de Legendre de grau 4;
+2) O cálculo das raízes do polinômio do ítem 1);
+3) Os polinômios de interpolação de Lagrange associados aos pontos amostrais do item 2);
+e o cálculo dos pesos associados aos pontos amostrais do item 2). */
