dd4a6ca465158fd4dd66fdea17b4767a47374a94
Thiago <thiagoisaiasbrg@gmail.com>
2016-04-21 00:42:35 -0300

    New Files - Gauss Quadratures

---
+++ 04_GaussHermite.cpp
@@ -0,0 +1,4 @@
+/* Implementar e testar a quadratura de Gauss-Hermite para integração adequada a esse tipo de quadratura.
+O método que retorna o valor da função para um dado x deve ser programado e incorporado ao código.
+A entrada de dados deve ser:
+1) o número de pontos de Hermite a serem usados no somatório (1 a 6). */
---
+++ 06_GaussChebyshev.cpp
@@ -0,0 +1,4 @@
+/*  Implementar e testar a quadratura de Gauss-Chebyshev para integração adequada a esse tipo de quadratura.
+O método que retorna o valor da função para um dado x deve ser programado e incorporado ao código.
+A entrada de dados deve ser:
+1) o número de pontos de Chebyshev a serem usados no somatório. */
