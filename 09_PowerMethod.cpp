c37a946417ea0eaf45ce8f25f6207d75f95fbd4c
Thiago <thiagoisaiasbrg@gmail.com>
2016-05-19 20:50:28 -0300

    Initial Files

---
+++ 09_PowerMethod.cpp
@@ -0,0 +1,5 @@
+/* Implementar e testar o método da Potência Regular. O código deve ler:
+Uma matriz nxn a partir de um arquivo (ler n, ler a matriz),
+Ler um vetor inicial
+Ler uma tolerância (epsilon) a ser usada no critério de parada.
+O programa deve imprimir em arquivo o autovalor dominante (Lambda_1) e o autovetor correspondente (Phi_1). */
---
+++ 10_InversePowerMethod.cpp
@@ -0,0 +1,2 @@
+/* Idêntica à tarefa T09, exceto que o código deve determinar o autovalor (Lambda_n - Menor autovalor em módulo)
+e seu autovetor correspondente (Phi_n).*/
